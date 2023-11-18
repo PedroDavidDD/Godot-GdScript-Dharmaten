bcf805e590374eb72be1cbae3efde6b3eee406cc
PedroDavid <u19207574@utp.edu.pe>
2023-11-18 02:29:26 -0500

    [upd-base-F]

--- scenes/game/characters/main_character/MainCharacterBomb.gd
+++ scenes/game/characters/main_character/MainCharacterBomb.gd
@@ -14,21 +14,14 @@ func _ready():
 	HealthDashboard.add_bomb(2)
 	_move_script = get_parent().get_node("MainCharacterMovement")
 
-
 # Called when the node enters the scene tree for the first time.
-func _unhandled_input(event):
+func _getBomb(bomb_scene, _character_position):
 	# Validamos cuantas bombas tenemos
 	var _count_bomb = HealthDashboard.points["Bomb"]
 	# Cuando se presiona la tecla (B - bomb) y no tiramos la bomba antes
-	if event.is_action_released("bomb") and not _move_script.bombing and _count_bomb > 0:
+	if not _move_script.bombing and _count_bomb > 0:
 		# Quitamos la bomba del invetario
 		HealthDashboard.add_bomb(-1)
-		# Inicializamos la bomba
-		var bomb_scene = _bomb.instantiate()
-		# Seteamos la posisión a la par del personaje principal
-		var _character = get_parent()
-		var _character_position = _character.position
-		bomb_scene.position = _character_position
 		# Seteamos la dirección de la fuerza y offset
 		if _move_script.turn_side == "right":
 			bomb_scene.linear_velocity.x = abs(bomb_scene.linear_velocity.x)
@@ -38,6 +31,3 @@ func _unhandled_input(event):
 			bomb_scene.linear_velocity.x = - abs(bomb_scene.linear_velocity.x)
 			bomb_scene.linear_velocity.y = abs(bomb_scene.linear_velocity.y) - 5
 			bomb_scene.position.x = _character_position.x - 25
-	
-		# Agregamos la bomba a la escena
-		get_parent().get_parent().add_child(bomb_scene)
--- scenes/game/characters/main_character/main_character_movement.gd
+++ scenes/game/characters/main_character/main_character_movement.gd
@@ -1,28 +1,24 @@
 extends Node2D
-## Controlador de movimiento y animación de un personaje.
-##
-## Detecta eventos de teclado para poder mover un personaje por un escenario
-## y ajustar animaciones según el movimiento
-## Movimiento básica de personaje: https://docs.google.com/document/d/1c9XXznR1KBJSr0jrEWjYIqfFuNGCGP2YASkXsFgEayU/edit
-class_name Player
 
-@export var character: CharacterBody2D # Referencia al personaje a mover
-@export var main_animation: AnimatedSprite2D # Referencia al sprite del personaje
+@onready var player = $".." # Referencia al personaje a mover
+@onready var main_animation := $"../Animation" # Referencia al sprite del personaje
 @export var effect_animation_sword: AnimatedSprite2D # Referencia al sprite del personaje
-@export var audio_player: AudioStreamPlayer2D # Reproductor de audios
+@onready var audio_player:= $"../AudioStreamPlayer2D" # Reproductor de audios
 @onready var _collision := $"../AreaSword/CollisionShape2D" # Colicionador de espada
 @onready var main_animation_effects= $"../Effects" # Sprite effectos Player
 @onready var weapon := $"../Weapon" # Scena del gancho
 
+@onready var MainCharacterBomb := $"../MainCharacterBomb"
+
 var gravity = 650 # Gravedad para el personaje
 var velocity = 100 # Velocidad de movimiento en horizontal
+var fast_velocity = 300
 var jump = 220 # Capacidad de salto, entre mayor el número más se puede saltar
 # Mapa de movimientos del personaje
 var _movements = {
 	IDLE = "default",
 	IDLE_WITH_SWORD = "idle_with_sword",
-	LEFT_WITH_SWORD = "left_with_sword",
-	RIGHT_WITH_SWORD = "run_with_sword",
+	RUN_WITH_SWORD = "run_with_sword",
 	JUMP_WITH_SWORD = "jump_with_sword",
 	FALL_WITH_SWORD = "fall_with_sword",
 	HIT_WITH_SWORD = "hit_with_sword",
@@ -69,82 +65,63 @@ var player_movement_speed: int = 100.0
 # Define una variable para controlar si la función player_to_ball está activa o no
 var isPlayerToBallActive = false
 
-# Función de inicialización
-func _ready():
-	main_animation.play(_current_movement)
-	# Si no hay un personaje, deshabilitamos la función: _physics_process
-	if not character:
-		set_physics_process(false)
+var direction = Vector2()
+var bomb = preload("res://scenes/game/levels/objects/damage_object/bomb/bomb.tscn")  # Ruta al recurso de la bomba
 
-# Función de ejecución de físicas
-func _physics_process(_delta):
+# Keys
+var btnUp = Input.is_action_pressed("arriba")
+var btnDown = Input.is_action_pressed("abajo")
+var btnLeft = Input.is_action_pressed("izquierda")
+var btnRight = Input.is_action_pressed("derecha")
+
+func _process(_delta):
 	_move(_delta)
 
-func _unhandled_input(event):
-	# Cuando se presiona la tecla x, atacamos	
-	if event.is_action_released("hit"):
-		character.velocity.x = 0
-		_current_movement = _movements.ATTACK
-	# Cuando se presiona la tecla b, lanzamos bomba
-	elif event.is_action_released("bomb"):
-		_current_movement = _movements.BOMB
-	_set_animation()
-
-# Función de movimiento general del personaje
 func _move(delta):
-	# Cuando se presiona la tecla (flecha izquierda), movemos el personaje a la izquierda
-	if Input.is_action_pressed("izquierda"):
-		character.velocity.x = -velocity
-		_current_movement = _movements.LEFT_WITH_SWORD	
-		turn_side = "left"
+	
+	# Keys
+	btnUp = Input.is_action_pressed("arriba")
+	btnDown = Input.is_action_pressed("abajo")
+	btnLeft = Input.is_action_pressed("izquierda")
+	btnRight = Input.is_action_pressed("derecha")
+	
+	direction = direction.normalized()
+	
+	# Movimiento del jugador
+	if btnUp or btnDown or btnLeft or btnRight:
+		if btnUp:
+			direction.y -= 1
+		elif btnDown:
+			direction.y += 1
 		
-		weapon.scale.x = -1
-	# Cuando se presiona la tecla (flecha derecha), movemos el personaje a la derecha
-	elif Input.is_action_pressed("derecha"):
-		character.velocity.x = velocity
-		_current_movement = _movements.RIGHT_WITH_SWORD
-		turn_side = "right"
-		
-		weapon.scale.x = 1
-	# Cuando no presionamos teclas, no hay movimiento	
+		if btnLeft:
+			direction.x -= 1
+		elif btnRight:
+			direction.x += 1
+		_current_movement = _movements.RUN_WITH_SWORD
 	else:
-		character.velocity.x = 0
 		_current_movement = _movements.IDLE
 	
-	# Cuando se presiona la tecla (espacio), hacemos animación de salto
-	if Input.is_action_just_pressed("saltar"):
-		if character.is_on_floor():
-			_current_movement = _movements.JUMP_WITH_SWORD
-			_is_jumping = true
-			_jump_count += 1 # Sumamos el primer salto
-		elif _is_jumping and _jump_count < _max_jumps:
-			_current_movement = _movements.JUMP_WITH_SWORD
-			_jump_count += 1 # Sumamos el segundo salto
+	# Movimiento normal o rápido al presionar la tecla shif
+	if Input.is_key_pressed(KEY_SHIFT):
+		move_player(fast_velocity, delta)
+	else:
+		move_player(velocity, delta)
 	
-	# Cuando se presiona la tecla (f), hacemos animación de Dash
-	if canTeleport and Input.is_action_just_pressed("teleport"):
-		_current_movement = _movements.TELEPORT
-		teleport()
+	# Disparar un gancho
+	if Input.is_action_pressed("isGancho"):
+		shoot_weapon()
 	
-	if Input.is_action_pressed("clic"):
-		if !isShootBall:
-			weapon.get_node("AnimatedSprite2D").play("pistola")
-			shootBall()
-			isShootBall = true
-			await get_tree().create_timer(0.5).timeout
-			isShootBall = false
+	# Cuando se presiona la tecla x, atacamos	
+	if Input.is_action_just_pressed("hit"):
+		print("atacando")
 	
-	SkillPlayerToBall(character, delta)
-	
-	_apply_gravity(delta)
-	
-	if _died: # Si el personaje murió, no se podrá mover en el eje X
-		character.velocity.x = 0
+	# Cuando se presiona la tecla b, lanzamos bomba
+	if Input.is_action_just_pressed("bomb"):
+		_current_movement = _movements.BOMB
+		throw_bomb()
 	
 	_set_animation()
-	# Función de godot para mover y aplicar física y colisiones
-	character.move_and_slide()
-
 
 # Controla la animación según el movimiento del personaje
 func _set_animation():
@@ -155,14 +132,6 @@ func _set_animation():
 	if _died:
 		main_animation.play(_movements.DEAD_HIT)
 		return
-	if _is_jumping:	
-		# Movimiento de salto (animación de "salto")
-		if character.velocity.y >= 0:
-			# Estamos cayendo
-			main_animation.play(_movements.FALL_WITH_SWORD)
-		else:
-			# Estamos subiendo
-			main_animation.play(_movements.JUMP_WITH_SWORD)
 	elif _current_movement == _movements.ATTACK:
 		# Atacamos
 		attacking = true
@@ -177,18 +146,14 @@ func _set_animation():
 		# Lanzamos bomba
 		bombing = true
 		main_animation.play(_movements.BOMB)
-	elif _current_movement == _movements.RIGHT_WITH_SWORD:
-		# Movimiento hacia la derecha (animación "correr" no volteada)
-		main_animation.play(_movements.RIGHT_WITH_SWORD)
-		main_animation.flip_h = false
-		_collision.position.x = abs(_collision.position.x)
-	elif _current_movement == _movements.LEFT_WITH_SWORD:
-		# Movimiento hacia la izquierda (animación "correr" volteada)
-		main_animation.play(_movements.RIGHT_WITH_SWORD)
-		main_animation.flip_h = true
-		_collision.position.x = - abs(_collision.position.x)
-	elif _current_movement == _movements.FLY:
-		main_animation.play(_movements.FLY)
+	elif _current_movement == _movements.RUN_WITH_SWORD:
+		if btnRight:
+			# Movimiento hacia la derecha (animación "correr" no volteada)
+			main_animation.flip_h = false
+		elif btnLeft:
+			# Movimiento hacia la izquierda (animación "correr" volteada)
+			main_animation.flip_h = true
+		main_animation.play(_movements.RUN_WITH_SWORD)
 	else:
 		# Movimiento por defecto (animación de "reposo")
 		main_animation.play(_movements.IDLE)
@@ -196,132 +161,6 @@ func _set_animation():
 		audio_player.stop()
 		_is_playing = ""
 
-func teleport():
-	_is_teleporting = true
-	canTeleport = false
-	# Reproducimos la animación del teleport
-	do_animation()
-	await get_tree().create_timer(1).timeout
-	_is_teleporting = false
-	await get_tree().create_timer(1.3).timeout
-	canTeleport = true
-	
-func do_animation():
-	_play_sound(_teleport_sound)
-	main_animation_effects.play(_movements.TELEPORT_EFFECT)
-	
-# Función que aplica gravedad de caída o salto
-func _apply_gravity(delta):
-	var v = character.velocity
-	
-	# El salto solo se ejecuta 1 vez, en ese momento hacemos que el personaje salte
-	if _current_movement == _movements.JUMP_WITH_SWORD and not _died:
-		# Saltamos, solo si el personaje no ha muerto
-		v.y = -jump
-	# El salto solo se ejecuta 1 vez, en ese momento hacemos que el personaje salte
-	elif _current_movement == _movements.TELEPORT and not _died:
-		if (v.x < 0):
-			v.x = -5 * teleportSpeed * gravity * delta
-		else:
-			v.x = 5 * teleportSpeed * gravity * delta
-		v.y = -teleportSpeed
-	else:
-		# Aplicación de gravedad (aceleración en la caida)
-		v.y += gravity * delta
-		# Después de un salto, validamos cuando volvemos a tocar el suelo para poder volver a saltar
-		if character.is_on_floor():
-			# Reseteamos variables de salto
-			_is_jumping = false
-			_jump_count = 0
-	
-	# Aplicamos el vector de velocidad al personaje
-	character.velocity = v
-
-func die():
-	# Seteamos la variable de morir averdadero
-	_died = true
-	GameManager.level_start = false
-
-# Recibir daño
-func hit(value: int):
-	if _died:
-		return
-	attacking = false
-	HealthDashboard.remove_life(value)
-	_play_sound(_male_hurt_sound)
-	main_animation.play("hit_with_sword")
-	
-	# Bajamos vida y validamos si el personaje ha perdido
-	if HealthDashboard.life == 0:
-		die()
-	else:
-		pass
-		# Animación de golpe
-
-
-func _on_animation_animation_finished():
-	# Validamos si la animación es de morir
-	if main_animation.get_animation() == 'dead_hit':
-		# Validamos si el sonido ya esta sonando
-		if _is_playing != "_dead_sound":
-			_is_playing = "_dead_sound"
-			# Reproducimos el sonido
-			_play_sound(_dead_sound)
-	elif main_animation.get_animation() == _movements.ATTACK:
-		attacking = false
-	elif main_animation.get_animation() == _movements.BOMB:
-		bombing = false
-
-
-func _on_animation_frame_changed():	
-	# Si la animación es de atacar habilitamos el colicionador
-	if main_animation.animation == "attack_2" and main_animation.frame == 1:
-		_collision.set_deferred("disabled", false)
-	else:
-		# Si la animación no es de atacar deshabilitamos el colicionador
-		_collision.set_deferred("disabled", true)
-		
-	if main_animation.animation == _movements.JUMP_WITH_SWORD:
-		# Validamos si el sonido ya esta sonando
-		if _is_playing != "_jump_sound":
-			_is_playing = "_jump_sound"
-			# Reproducimos el sonido
-			_play_sound(_jump_sound)
-	if (
-		main_animation.animation == _movements.RIGHT_WITH_SWORD 
-		or main_animation.animation == _movements.LEFT_WITH_SWORD 
-	):
-		# Validamos si el sonido ya esta sonando
-		if _is_playing != "_run_sound":
-			_is_playing = "_run_sound"
-			# Reproducimos el sonido
-			_play_sound(_run_sound)
-
-
-func _on_audio_stream_player_2d_finished():
-	if audio_player.stream == _dead_sound:
-		# Qitamos al personaje principal de la excena
-		self.get_parent().queue_free()
-		# Reiniciamos el juego despues de 2 segundos
-		SceneTransition.reload_scene()
-		
-		
-func _play_sound(sound):
-	# Pausamos el sonido
-	audio_player.stop()
-	# Reproducimos el sonido
-	audio_player.stream = sound
-	audio_player.play()
-	
-func set_disabled(disabled: bool):
-	set_physics_process(not disabled)
-	
-func set_idle():
-	# Movimiento por defecto (animación de "reposo")
-	main_animation.play(_movements.IDLE_WITH_SWORD)
-	# Pausamos el sonido
-	audio_player.stop()
-	
 func _play_sword_effect():
 	# Obtenems que efecto tenemos activo
 	var type = Global.attack_effect
@@ -338,49 +177,29 @@ func _play_sword_effect():
 	# Reproducimos el efecto de la espada
 	effect_animation_sword.play("attack_2_effect")
 
-func shootBall():
-	var shoot_ball = weapon.ball.instantiate()
-	if turn_side == "left":
-		shoot_ball.scale = Vector2(-0.5, 0.5)
-		shoot_ball.speed = -320
-	else:
-		shoot_ball.scale = Vector2(0.5, 0.5)
-		shoot_ball.speed = 320
-	shoot_ball.global_position = weapon.get_node("Direction").global_position
-	get_tree().call_group("scene_0", "add_child", shoot_ball)
+func _play_sound(sound):
+	# Pausamos el sonido
+	audio_player.stop()
+	# Reproducimos el sonido
+	audio_player.stream = sound
+	audio_player.play()
 
-func player_to_ball(character, delta):
-	var player_ball = get_parent().get_parent().get_node("Ball")
-	if player_ball and canPlayerMove:
-		var ultima_ball = get_tree().get_nodes_in_group("ballGroup").back()
-		var target_position = ultima_ball.global_position
-		var distance_threshold = 20
+# Función para mover al jugador
+func move_player(speed, delta):
+	player.position += direction * speed * delta
 
-		var direction = (target_position - character.global_position).normalized()
-		var move_amount = direction * player_movement_speed * delta
-		
-		if character.global_position.distance_to(target_position) > distance_threshold:
-			character.global_position += move_amount
-			# Desactivar la gravedad en el eje Y
-			character.velocity.y = 0
-			_current_movement = _movements.FLY
-			weapon.get_node("AnimatedSprite2D").self_modulate = Color("#c5c5c500")
-		else:
-			canPlayerMove = false
-			ultima_ball.is_ball_moving = true
-			_current_movement = _movements.IDLE
-			weapon.get_node("AnimatedSprite2D").self_modulate = Color("#ffffff")
-			ultima_ball.queue_free()
+# Función para disparar un gancho
+func shoot_weapon():
+	var new_weapon = weapon.ball.instantiate()
+	new_weapon.position = player.position
+	get_tree().call_group("scene_0", "add_child", new_weapon)
 
-func SkillPlayerToBall(character, delta):
-	# Verifica si se presiona la tecla 'G'
-	if Input.is_action_pressed("isGancho") && !_is_jumping:
-		# Activa o desactiva la función player_to_ball
-		isPlayerToBallActive = not isPlayerToBallActive
-	# Ejecuta la función player_to_ball si la variable isPlayerToBallActive está en True
-	if isPlayerToBallActive:
-		player_to_ball(character, delta)
-		HealthDashboard.label_SkillGancho.text = "[1]"
-	else:
-		HealthDashboard.label_SkillGancho.text = "[0]"
-		weapon.get_node("AnimatedSprite2D").self_modulate = Color("#ffffff")
+	# Tirar bombas
+func throw_bomb():
+	var new_bomb = bomb.instantiate()
+	# Seteamos la posisión a la par del personaje principal
+	var _character_position = player.position
+	new_bomb.position = _character_position
+	MainCharacterBomb._getBomb(new_bomb, _character_position)	
+	# Agregamos la bomba a la escena
+	get_tree().call_group("scene_0", "add_child", new_bomb)
