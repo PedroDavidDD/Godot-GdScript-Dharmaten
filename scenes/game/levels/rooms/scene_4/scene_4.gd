60c1fdb506ce3057df79669a08011b526ea616dd
PedroDavid <u19207574@utp.edu.pe>
2023-12-01 23:06:59 -0500

    [upd-Scenes
    + Scenas totales: 5
    + Scena 5: botón de reiniciar

--- scenes/game/levels/rooms/scene_4/scene_4.gd
+++ scenes/game/levels/rooms/scene_4/scene_4.gd
@@ -1,8 +1,7 @@
 extends Node2D
+
 var player = []
-var enemy = load("res://scenes/game/enemy/Slime/slime_green.tscn")
-var wave = 1
-var rng = RandomNumberGenerator.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_tree().get_nodes_in_group("player")[0]
@@ -12,21 +11,5 @@ func _ready():
 	player.global_position = GameManager.last_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
 	pass
-
-
-func _on_spawn_timer_timeout():
-	wave +=1
-	var random_number = rng.randi_range(1,4)
-	var inst=enemy.instantiate()
-	
-	if (random_number == 1):
-		inst.position = $SpawnLocation1.position
-	elif (random_number == 2):
-		inst.position = $SpawnLocation2.position
-	elif (random_number == 3):
-		inst.position = $SpawnLocation3.position
-	else:
-		inst.position = $SpawnLocation4.position	
-	add_child(inst)
